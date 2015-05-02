3b03bd22f21425bbd3eb3d0748b3f9945ac0e5a7
anlorn <anlorn@anlorn.com>
2015-05-02 20:16:23 +0300

    Initial

---
+++ src/com/example/carservicing/MainMenuActivity.java
@@ -0,0 +1,15 @@
+package com.example.carservicing;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class MainMenuActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
